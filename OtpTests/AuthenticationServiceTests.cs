fd94a8261ad93d647f5e2303275e2a10ff6bac17
Joey Chen <jay.hatelove@gmail.com>
2018-09-22 23:23:13 +0800

    otp with .gitignore

---
+++ OtpTests/AuthenticationServiceTests.cs
@@ -0,0 +1,18 @@
+using System;
+using OtpLib;
+using Xunit;
+
+namespace OtpTests
+{
+    public class AuthenticationServiceTests
+    {
+        [Fact]
+        public void IsValidTest()
+        {
+            var target = new AuthenticationService();
+
+            var actual = target.IsValid("joey", "91000000");
+            Assert.True(actual);
+        }
+    }
+}
