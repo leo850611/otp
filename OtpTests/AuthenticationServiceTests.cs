752c5e3c7a4f67bb4cdb0d97f6693e4081abf45d
LeoSheu <leo850611@hotmail.com>
2019-01-12 14:17:12 +0800

    refactor

--- OtpTests/AuthenticationServiceTests.cs
+++ OtpTests/AuthenticationServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using NSubstitute;
 using NUnit.Framework;
 using OtpLib;
@@ -7,18 +8,53 @@ namespace OtpTests
 {
     public class AuthenticationServiceTests
     {
+        private static IToken _rsaTokenDao = Substitute.For<IToken>();
+        private static IProfile _givenProfile = Substitute.For<IProfile>();
+
         [Test]
         public void IsValidTest()
         {
-            var profileDao = Substitute.For<IProfile>();
-            profileDao.GetPassword("joey").Returns("91");
+            GivenProfile("joey", "91");
+            GivenToken("000000");
+            ShouldValid("joey", "91000000");
+        }
 
-            var rsaTokenDao = Substitute.For<IToken>();
-            rsaTokenDao.GetRandom("").ReturnsForAnyArgs("000000");
+        [Test]
+        public void IsInValidTest()
+        {
+            GivenProfile("joey", "91");
+            GivenToken("123456");
+            ShouldInValid("joey", "91000000");
+        }
 
-            var target = new AuthenticationService(profileDao, rsaTokenDao);
-            var actual = target.IsValid("joey", "91000000");
-            Assert.True(actual);
+
+        private static void ShouldInValid(string account, string passcode)
+        {
+            var target = new AuthenticationService(_givenProfile, _rsaTokenDao);
+            Assert.False(target.IsValid(account, passcode));
+        }
+        private static void ShouldValid(string account, string passcode)
+        {
+            var target = new AuthenticationService(_givenProfile, _rsaTokenDao);
+            Assert.True(target.IsValid(account, passcode));
+        }
+
+        private static IToken GivenToken(string token)
+        {
+            _rsaTokenDao.GetRandom("").ReturnsForAnyArgs(token);
+            return _rsaTokenDao;
+        }
+
+        private static IProfile GivenProfile(string account, string returnThis)
+        {
+            _givenProfile.GetPassword(account).Returns(returnThis);
+            return _givenProfile;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+
         }
     }
 
